c3c5b7f05576b0f6db8f19f8f5ebda24d1348e28
Sophtli <mail@sopht.li>
2024-09-13 00:11:13 +0200

    update discord rpc

--- scripts/discord.gd
+++ scripts/discord.gd
@@ -1,9 +1,9 @@
 extends Node
 
 func _ready() -> void:
-	DiscordRPC.app_id = 985579917406265354 # Application ID
-	DiscordRPC.details = "3DSpiel"
-	DiscordRPC.state = "Some State"
+	DiscordRPC.app_id = 1283912005064851486 # Application ID
+	DiscordRPC.details = "in Godot"
+	DiscordRPC.state = "am Entwickeln"
 	# DiscordRPC.large_image = "example_game" # Image key from "Art Assets"
 	# DiscordRPC.large_image_text = "Try it now!"
 	# DiscordRPC.small_image = "boss" # Image key from "Art Assets"
